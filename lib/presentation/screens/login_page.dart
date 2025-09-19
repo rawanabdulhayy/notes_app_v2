6211038193dd9eed3487b6f5ae0ef960ab77cd6d
rawanabdulhayy <rawanabdulhay29700@gmail.com>
2025-09-19 20:31:18 +0300

    login page, and signup page all set, and firebase console setup all done

---
+++ lib/presentation/screens/login_page.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app_using_firebase/core/app_colors/AppColors.dart';
+import 'package:notes_app_using_firebase/presentation/screens/sign_up_page.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 135, left: 45, right: 45),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Hi, Welcome Back!",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 25,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Email",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 5),
+                TextField(
+                  style: TextStyle(color: Colors.white), //Input Text Color
+                  //Decoration - label and hintText
+                  decoration: InputDecoration(
+                    // //Label
+                    // labelText: "Email",
+                    // labelStyle: TextStyle(color: Colors.white),
+                    //Hint Text
+                    hintText: "example@gmail.com",
+                    hintStyle: TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 0.7),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
+                    ),
+                    //background color
+                    fillColor: AppColors.secondaryColor,
+                    filled: true,
+                    //border
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  //extras about textFields:
+                  //     enabledBorder: Border when not focused.
+                  // focusedBorder: Border when focused.
+                  // errorBorder: Border when error is shown.
+                  // focusedErrorBorder: Border when error + focused.
+                  // border: Default border if none of the above apply.
+                  // All use OutlineInputBorder or UnderlineInputBorder.
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "Password",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 5),
+                TextField(
+                  style: TextStyle(color: Colors.white), //Input Text Color
+                  //Decoration - label and hintText
+                  decoration: InputDecoration(
+                    // //Label
+                    // labelText: "Email",
+                    // labelStyle: TextStyle(color: Colors.white),
+                    //Hint Text
+                    hintText: "Enter Your Password",
+                    hintStyle: TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 0.7),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
+                    ),
+                    //background color
+                    fillColor: AppColors.secondaryColor,
+                    filled: true,
+                    //border
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  //password specific obscuring
+                  obscureText: true,
+                ),
+              ],
+            ),
+            SizedBox(height:80,),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    //width and height in elevated buttons -- denoted by minimum size property (width, height)
+                    minimumSize: Size(312, 48),
+                    backgroundColor: Colors.white, // button background color
+                    foregroundColor:
+                        AppColors.primaryColor, // text (and icon) color
+                    // In textField, borderRadius was nested under the border property, nested under InputDecoration.
+                    // In elevated button, borderRadius was nested under the shape property, nested under ElevatedButton,styleFrom.
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5), // rounded corners
+                    ),
+                  ),
+                  child: Text("Login"),
+                ),
+                SizedBox(height: 12),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    //width and height in elevated buttons -- denoted by minimum size property (width, height)
+                    minimumSize: Size(312, 48),
+                    backgroundColor: Colors.white, // button background color
+                    foregroundColor:
+                        AppColors.primaryColor, // text (and icon) color
+                    // In textField, borderRadius was nested under the border property, nested under InputDecoration.
+                    // In elevated button, borderRadius was nested under the shape property, nested under ElevatedButton,styleFrom.
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5), // rounded corners
+                    ),
+                  ),
+                  child: Text("Continue with Google"),
+                ),
+              ],
+            ),
+            SizedBox(height: 230,),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: GestureDetector(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (_) => SignUpPage(),
+                  ));
+                },
+                child: Text(
+                  'Don\'t have an account? Sign Up',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
